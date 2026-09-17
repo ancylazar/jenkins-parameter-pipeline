ddf9fe3fa4cb5dfb8496ce6e5f378bd0ad87d55e
ancylazar <ancylazar2105@gmail.com>
2026-09-17 22:24:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 with open("report.txt", "w")as f:
     f.write("ApplicationReport\n")
-    f.write("TotalUsers:120\n")
+    f.write("TotalUsers:200\n")
     f.write("ActiveSessions:45\n")
     print("Reportgenerated.")
